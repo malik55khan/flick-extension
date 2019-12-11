267b16a726f386188f669899643bde0dfd0139dd
malik55khan <malik55khan@gmail.com>
2019-12-11 11:58:38 -0500

    code updated

--- config/db.js
+++ config/db.js
@@ -20,7 +20,7 @@ exports.connect = function () {
     
     // If the connection throws an error
     db.on('error', function (err) {
-        logger.log('error', 'Mongoose default connection error: %s', err);
+        console.log('error', 'Mongoose default connection error: %s', err);
     });
     process.on('SIGINT', function () {
         mongoose.connection.close(function () {
