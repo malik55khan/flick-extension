ae28dfb6664cf48ffe8878b2482433fb84dcb701
malik55khan <malik55khan@gmail.com>
2020-01-02 00:59:55 +0530

    code updated

--- modules/notification/notification.ctrl.js
+++ modules/notification/notification.ctrl.js
@@ -32,6 +32,34 @@ const getMyNotifications = async (req, res, next) => {
     }
   }
 }
+const getMyPendingNotifications = async (req, res, next) => {
+  try {
+    let id = ObjectId(req.access_token._id);
+    let conditions = {
+      query:{
+        'userId':id,
+        "isNotify":false
+      }
+    };
+    
+    let aggregate = sharedService.bindQuery(conditions);
+    console.log(aggregate);
+    let data = await notiServiceProvider.getAll(aggregate);
+    console.log(data)
+    res.status(200)
+      .json({
+        code: 200,
+        status: "success",
+        data: data,
+        message: serverMessages.SUCCESS_FOUND
+      });
+  }catch(err){
+    console.log(err);
+    if (err) {
+      return next(Boom.badImplementation(err.message));
+    }
+  }
+}
 const create = async (req,res)=>{
   try{
     let body = req.body;
@@ -51,7 +79,33 @@ const create = async (req,res)=>{
     }
   }
 }
+const notified = async (req,res)=>{
+  try{
+    let body = req.body;
+    let conditions = {
+      //userId:ObjectId(req.access_token._id),
+      _id:ObjectId(req.params.id),
+      
+    };
+    
+    let notification = await notiServiceProvider.update(conditions,{isNotify:true});
+    res.status(200)
+      .json({
+        code: 200,
+        status: 'success',
+        data: notification,
+        message: serverMessages.SUCCESS_UPDATED
+      });
+  }catch(err){
+    console.log(err);
+    if (err) {
+      return next(Boom.badImplementation(err.message));
+    }
+  }
+}
 module.exports = {
   getMyNotifications: getMyNotifications,
-  create:create
+  create:create,
+  notified:notified,
+  getMyPendingNotifications:getMyPendingNotifications
 };
--- modules/notification/notification.route.js
+++ modules/notification/notification.route.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var ctrl = require('./notification.ctrl');
 var jwt = require('../../config/jwToken');
 router.route(`/`).post(jwt.isAuthorizedToken,ctrl.create);
+router.route(`/pending`).get(jwt.isAuthorizedToken,ctrl.getMyPendingNotifications);
+router.route(`/notified/:id`).put(jwt.isAuthorizedToken,ctrl.notified);
 router.route(`/`).get(jwt.isAuthorizedToken,ctrl.getMyNotifications); //- get a list of tribes that member belongs to
 module.exports = router;
